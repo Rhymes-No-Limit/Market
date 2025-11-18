b6aae97c249e17db54ab473f3ca949069b1d8ee3
Андрей <Rhymes.un.limit@gmail.com>
2025-11-18 23:34:46 +0300

    add warehouse

---
+++ Market/store.swift
@@ -0,0 +1,8 @@
+//
+//  store.swift
+//  Market
+//
+//  Created by Андрей on 18.11.2025.
+//
+
+import Foundation
---
+++ Market/warehouse.swift
@@ -0,0 +1,8 @@
+//
+//  warehouse.swift
+//  Market
+//
+//  Created by Андрей on 18.11.2025.
+//
+
+import Foundation
